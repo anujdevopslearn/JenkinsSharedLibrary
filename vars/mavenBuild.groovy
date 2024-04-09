a7facd34f3165d992d7016e0665bca89ffe9d45d
Your Name <anuj_sharma401@yahoo.com>
2024-04-09 15:46:21 +0530

    Added Maven Groovy Files

---
+++ vars/mavenBuild.groovy
@@ -0,0 +1,42 @@
+def call(Map config = [:]){
+    def gitURL = config.gitURL
+    def productType = config.productType
+    pipeline {
+        agent any
+        environment {
+            gitBranch = "${env.BRANCH_NAME}"
+            pomfileName = config.pomfileName
+        }
+        stages{
+            stage("Code Checkout"){
+                steps{
+                    git changelog: false, poll: false, url: gitURL, branch: gitBranch
+                }
+            }
+            stage("Code Build"){
+                steps{
+                    script {
+                        sh """
+                            ls -lart
+                            mvn clean install
+                        """
+                    }
+                }
+            }
+            stage("Docker Image Build"){
+                when {
+                    expression {
+                        productType == "docker"
+                    }
+                }
+                steps{
+                    script {
+                        sh """
+                            echo "Docker Image Build"
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
