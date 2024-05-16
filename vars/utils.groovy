d9557f21a2017eeb410fde33344aa2d4beb390c2
Anuj Sharma <anuj_sharma401@yahoo.com>
2024-05-16 16:06:07 +0530

    Update utils.groovy

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,5 +1,10 @@
 def dockerImageBuild(containerName, tag){
     sh "docker image prune -f"
     sh "docker build -t $containerName:$tag --pull --no-cache ."
+    withCredentials([usernamePassword(credentialsId: 'DockerHubCreds', passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
+        sh "docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD"
+        sh "docker tag $containerName:$tag $DOCKERHUB_USERNAME/$containerName:$tag"
+        sh "docker push $DOCKERHUB_USERNAME/$containerName:$tag"
+    }
     echo "Image build complete"
 }
